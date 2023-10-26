2d5cca0a58e1d3ca874fbf80d63fae77f846c9e5
AlexHunter15 <134641085+AlexHunter15@users.noreply.github.com>
2023-10-26 18:46:45 +0300

    Add files via upload

---
+++ Processes/LR2(2).cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <vector>
+#include <sstream>
+#include <windows.h>
+
+bool IsPrime(int num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 == 0 || num % 3 == 0) return false;
+    for (int i = 5; i * i <= num; i += 6) {
+        if (num % i == 0 || num % (i + 2) == 0) return false;
+    }
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "Russian");
+
+    if (argc < 2) {
+        std::cerr << "Необходимо передать количество элементов и элементы массива через аргументы командной строки." << std::endl;
+        return 1;
+    }
+
+    int n;
+    std::istringstream(argv[1]) >> n;
+
+    std::vector<int> arr(n);
+
+    for (int i = 0; i < n; ++i) {
+        std::istringstream(argv[i + 2]) >> arr[i];
+    }
+
+    std::vector<int> primes;
+    std::vector<int> nonPrimes;
+
+    for (int i = 0; i < n; ++i) {
+        if (IsPrime(arr[i])) {
+            primes.push_back(arr[i]);
+        } else {
+            nonPrimes.push_back(arr[i]);
+        }
+    }
+
+    std::cout << "Простые числа: ";
+    for (int prime : primes) {
+        std::cout << prime << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Непростые числа: ";
+    for (int nonPrime : nonPrimes) {
+        std::cout << nonPrime << " ";
+    }
+    std::cout << std::endl;
+    int a;
+    std::cin >> a;
+    return 0;
+}
+
---
+++ Processes/LR2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    SetConsoleTitle(L"Parent Window");
+
+    int n;
+    std::cout << "Введите количество элементов: ";
+    std::cin >> n;
+
+    std::string Command = "LR2(2).exe " + std::to_string(n);
+
+    for (int i = 0; i < n; ++i) {
+        int element;
+        std::cout << "Введите элемент " << i + 1 << ": ";
+        std::cin >> element;
+        Command += " " + std::to_string(element);
+    }
+
+    const char* command = Command.c_str();
+    STARTUPINFOW si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(STARTUPINFOW));
+    si.cb = sizeof(STARTUPINFOW);
+
+    si.dwFlags = STARTF_USESHOWWINDOW;
+    si.wShowWindow = SW_SHOWMINIMIZED;
+    si.cb = sizeof(STARTUPINFO);
+    si.lpTitle = (LPWSTR)L"Child";
+    int len = strlen(command) + 1;
+    int size = MultiByteToWideChar(CP_UTF8, 0, command, len, NULL, 0);
+    wchar_t* wCommand = new wchar_t[size];
+
+    if (CreateProcess(NULL, (LPWSTR)wCommand, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi)) {
+        WaitForSingleObject(pi.hProcess, INFINITE);
+    }
+    else {
+        std::cerr << "Ошибка при создании процесса Child" << std::endl;
+        return 1;
+    }
+}
