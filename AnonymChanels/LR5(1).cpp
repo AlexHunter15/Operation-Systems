1384236c2d01f20d2a90dc934763697572e3d6b2
Aliaksei Khadyka <lesha15x@yandex.ru>
2023-11-29 17:36:35 +0300

    Add files via upload

---
+++ AnonymChanels/LR5(1).cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int sizeOfArray;
+    cout << "Input size of array: ";
+    cin >> sizeOfArray;
+
+    HANDLE hWritePipe, hReadPipe;
+    SECURITY_ATTRIBUTES sa;
+    sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+    sa.lpSecurityDescriptor = NULL;
+    sa.bInheritHandle = TRUE;
+
+    CreatePipe(&hReadPipe, &hWritePipe, &sa, 0);
+
+    STARTUPINFO stp;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&stp, sizeof(STARTUPINFO));
+    stp.cb = sizeof(STARTUPINFO);
+    wchar_t commandLine[80];
+    wsprintf(commandLine, L"Simple.exe %d %d", (int)hWritePipe, (int)hReadPipe);
+    CreateProcess(NULL, commandLine, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &stp, &pi);
+
+    DWORD dwBytesWritten;
+    WriteFile(hWritePipe, &sizeOfArray, sizeof(int), &dwBytesWritten, NULL);
+
+    vector<int> array(sizeOfArray);
+
+    CloseHandle(hWritePipe);
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+    DWORD dwBytesRead;
+    int sizeOfNewArray;
+    ReadFile(hReadPipe, &sizeOfNewArray, sizeof(int), &dwBytesRead, NULL);
+    vector<int> resultArray(sizeOfNewArray);
+
+
+    cout << "Received Prime Numbers from Simple Process: ";
+    for (int i = 0; i < sizeOfNewArray; ++i) {
+        ReadFile(hReadPipe, &resultArray[i], sizeof(int), &dwBytesRead, NULL);
+        cout << resultArray[i] << " ";
+    }
+    cout << endl;
+
+    CloseHandle(hReadPipe);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    return 0;
+}
---
+++ AnonymChanels/Simple.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <vector>
+
+using namespace std;
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    HANDLE hWritePipe = (HANDLE)atoi(argv[1]), hReadPipe = (HANDLE)atoi(argv[2]);
+
+    int size;
+    DWORD dwBytesRead;
+    ReadFile(hReadPipe, &size, sizeof(int), &dwBytesRead, NULL);
+    vector<int> arr(size);
+    cout << "Generated array"<<endl;
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 100;
+        cout << arr[i] << endl;
+    }
+    cout << "Primes array"<<endl;
+    vector<int> primes;
+    for (int i = 0; i < size; ++i) {
+        if (isPrime(arr[i])) {
+            primes.push_back(arr[i]);
+            cout << arr[i] << endl;
+        }
+    }
+
+    int primesCount = primes.size();
+    WriteFile(hWritePipe, &primesCount, sizeof(int), &dwBytesRead, NULL);
+
+    for (int i = 0; i < primesCount; ++i) {
+        WriteFile(hWritePipe, &primes[i], sizeof(int), &dwBytesRead, NULL);
+    }
+    CloseHandle(hWritePipe);
+    CloseHandle(hReadPipe);
+    int a;
+    cin >> a;
+    return 0;
+}
