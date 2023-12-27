63c567f390871e8a3870317904adda9633778500
Aliaksei Khadyka <lesha15x@yandex.ru>
2023-12-27 17:56:50 +0300

    Add files via upload

---
+++ LectureLabs/CreatingProcesses/Creator.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct employee {
+    int num;
+    char name[10];
+    double hours;
+};
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cout << "Usage: Creator <filename> <number_of_records>" << endl;
+        return 1;
+    }
+
+    const char* filename = argv[1];
+    int numRecords = atoi(argv[2]);
+
+    ofstream file(filename, ios::binary);
+    if (!file.is_open()) {
+        cout << "Error opening file." << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < numRecords; i++) {
+        employee emp;
+        cout << "Enter employee number: ";
+        cin >> emp.num;
+        cout << "Enter employee name (up to 10 characters): ";
+        cin >> emp.name;
+        cout << "Enter hours worked: ";
+        cin >> emp.hours;
+        file.write((char*)&emp, sizeof(employee));
+    }
+
+    file.close();
+    cout << "File " << filename << " created successfully." << endl;
+
+    return 0;
+}
---
+++ LectureLabs/CreatingProcesses/Reporter.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+struct employee {
+    int num;
+    char name[10];
+    double hours;
+};
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        cout << "Usage: Reporter <binary_filename> <report_filename> <hourly_rate>" << endl;
+        return 1;
+    }
+
+    const char* binaryFilename = argv[1];
+    const char* reportFilename = argv[2];
+    double hourlyRate = atof(argv[3]);
+
+    ifstream binaryFile(binaryFilename, ios::binary);
+    if (!binaryFile.is_open()) {
+        cout << "Error opening binary file." << endl;
+        return 1;
+    }
+
+    ofstream reportFile(reportFilename);
+    if (!reportFile.is_open()) {
+        cout << "Error opening report file." << endl;
+        return 1;
+    }
+
+    reportFile << "Отчет по файлу \"" << binaryFilename << "\"" << endl;
+    reportFile << "Номер сотрудника, имя сотрудника, часы, зарплата" << endl;
+
+    employee emp;
+    while (binaryFile.read((char*)&emp, sizeof(employee))) {
+        double salary = emp.hours * hourlyRate;
+        reportFile << emp.num << ", " << emp.name << ", " << emp.hours << ", " << fixed << setprecision(2) << salary << endl;
+    }
+
+    binaryFile.close();
+    reportFile.close();
+    cout << "Report file " << reportFilename << " created successfully." << endl;
+
+    return 0;
+}
